422df858c2873ca96b354e28f64f68276a724f72
Dan MacLean <maclean.daniel@gmail.com>
2012-11-21 21:28:11 +0000

    edit

--- examples/test.rb
+++ examples/test.rb
@@ -6,6 +6,8 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
   require 'bio-synreport'
   require 'pp'
   
+  #this is how you use it...
+  
   db = Bio::Util::SynReport.new(:gff => ARGV[0], :fasta => ARGV[1], :verbose => true)
   chr, pos, ref,alt = 'Chr2',7634495, 'a', 't'
   pp db.mutation_info(chr,pos,alt) 
